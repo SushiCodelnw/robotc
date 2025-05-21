bfadf2b801b7b6e171fd70bfbf4ef54eacf38e4b
SushiCodelnw <1389Dead@gmail.com>
2025-05-21 17:46:22 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
-def on_received_value(name, value):
-    if name == "Base":
-        pass
-    elif name == "Arm1":
-        pass
-    elif name == "Arm2":
-        pass
-    elif name == "Hand":
-        pass
-radio.on_received_value(on_received_value)
+def Left():
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 255, DigitalPin.P13, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 50, DigitalPin.P15, 0)
+def TurnLeft():
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 255, DigitalPin.P13, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 200, DigitalPin.P15, 1)
 
+def on_button_pressed_a():
+    global Start
+    Start = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def INahHee():
+    global Dis1, Dis2
+    while True:
+        Dis1 = pins.digital_read_pin(DigitalPin.P8)
+        Dis2 = pins.digital_read_pin(DigitalPin.P1)
+        if Dis1 == 0 and Dis2 == 0:
+            Forword()
+        elif Dis1 == 1 and Dis2 == 0:
+            Left()
+        elif Dis1 == 0 and Dis2 == 1:
+            Right()
+        elif Dis1 == 1 and Dis2 == 1:
+            Stop()
+            break
+def Right():
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 50, DigitalPin.P13, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 255, DigitalPin.P15, 0)
 def Forword():
-    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 120, DigitalPin.P13, 0)
-    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 120, DigitalPin.P15, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 120, DigitalPin.P13, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 120, DigitalPin.P15, 0)
+def Stop():
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P14, 0, DigitalPin.P13, 1)
+    RoboticsWorkshop.dd_mmotor(AnalogPin.P16, 0, DigitalPin.P15, 0)
+Dis2 = 0
+Dis1 = 0
+Start = 0
 radio.set_group(3)
+Start = 0
+Stop()
 
 def on_forever():
-    if pins.digital_read_pin(DigitalPin.P1) == 0 and pins.digital_read_pin(DigitalPin.P8) == 0:
-        pass
-    elif pins.digital_read_pin(DigitalPin.P1) == 1 and pins.digital_read_pin(DigitalPin.P8) == 0:
-        pass
-    elif pins.digital_read_pin(DigitalPin.P1) == 0 and pins.digital_read_pin(DigitalPin.P8) == 1:
-        pass
+    if Start == 1:
+        INahHee()
+        Forword()
+        basic.pause(200)
+        Stop()
+        INahHee()
+        TurnLeft()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,154 @@
 function Left () {
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P14,
-    200,
+    255,
     DigitalPin.P13,
     1
+    )
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P16,
+    40,
+    DigitalPin.P15,
+    0
+    )
+}
+function HandUp () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P2,
+    255,
+    DigitalPin.P12,
+    1
+    )
+    basic.pause(700)
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P2,
+    0,
+    DigitalPin.P12,
+    1
+    )
+}
+function TurnLeftFast () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P14,
+    255,
+    DigitalPin.P13,
+    1
+    )
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P16,
+    255,
+    DigitalPin.P15,
+    1
+    )
+    basic.pause(200)
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 1 && Dis2 == 0) {
+            break;
+        }
+    }
+    Stop()
+}
+function TurnLeftSlow () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P14,
+    100,
+    DigitalPin.P13,
+    1
+    )
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P16,
+    100,
+    DigitalPin.P15,
+    1
+    )
+    basic.pause(200)
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 1 && Dis2 == 0) {
+            break;
+        }
+    }
+    Stop()
+}
+function Forback () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P14,
+    120,
+    DigitalPin.P13,
+    0
+    )
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P16,
+    120,
+    DigitalPin.P15,
+    1
+    )
+}
+input.onButtonPressed(Button.A, function () {
+    HandUp()
+    Gofront()
+    Forword()
+    basic.pause(200)
+    for (let index = 0; index < 2; index++) {
+        Gofront()
+        Forword()
+        basic.pause(200)
+        TurnLeftFast()
+        Gofront()
+        HandDown()
+        TurnBack()
+        Gofront()
+        Forword()
+        basic.pause(300)
+        Stop()
+        HandUp()
+        Forback()
+        basic.pause(200)
+        TurnLeftSlow()
+    }
+    HandDown()
+    Gofront()
+    if (end == 1) {
+        TurnLeftFast()
+        Right()
+        basic.pause(1000)
+    } else if (end == 2) {
+        Forword()
+        basic.pause(100)
+        TurnRightFast()
+        Forword()
+        basic.pause(250)
+        Left()
+        basic.pause(800)
+    }
+    Gofront()
+})
+function Gofront () {
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 0 && Dis2 == 0) {
+            Forword()
+        } else if (Dis1 == 1 && Dis2 == 0) {
+            Left()
+        } else if (Dis1 == 0 && Dis2 == 1) {
+            Right()
+        } else if (Dis1 == 1 && Dis2 == 1) {
+            Stop()
+            basic.pause(500)
+            break;
+        }
+    }
+}
+function TurnRightFast () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P14,
+    200,
+    DigitalPin.P13,
+    0
     )
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P16,
@@ -11,34 +156,63 @@ function Left () {
     DigitalPin.P15,
     0
     )
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 0 && Dis2 == 1) {
+            break;
+        }
+    }
+    Stop()
 }
-function TurnLeft () {
-    RoboticsWorkshop.DDMmotor(
-    AnalogPin.P14,
-    255,
-    DigitalPin.P13,
-    1
-    )
-    RoboticsWorkshop.DDMmotor(
-    AnalogPin.P16,
-    200,
-    DigitalPin.P15,
-    1
-    )
-}
-input.onButtonPressed(Button.A, function () {
-    Start = 1
+input.onButtonPressed(Button.B, function () {
+    end += 1
+    if (end >= 3) {
+        end = 0
+    }
 })
+function HandDown () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P2,
+    255,
+    DigitalPin.P12,
+    0
+    )
+    basic.pause(1000)
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P2,
+    0,
+    DigitalPin.P12,
+    0
+    )
+}
+function Goback () {
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 0 && Dis2 == 0) {
+            Forback()
+        } else if (Dis1 == 1 && Dis2 == 0) {
+            Left()
+        } else if (Dis1 == 0 && Dis2 == 1) {
+            Right()
+        } else if (Dis1 == 1 && Dis2 == 1) {
+            Stop()
+            basic.pause(500)
+            break;
+        }
+    }
+}
 function Right () {
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P14,
-    255,
+    56,
     DigitalPin.P13,
     1
     )
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P16,
-    200,
+    255,
     DigitalPin.P15,
     0
     )
@@ -57,12 +231,35 @@ function Forword () {
     0
     )
 }
+function TurnBack () {
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P14,
+    255,
+    DigitalPin.P13,
+    0
+    )
+    RoboticsWorkshop.DDMmotor(
+    AnalogPin.P16,
+    255,
+    DigitalPin.P15,
+    0
+    )
+    basic.pause(500)
+    while (true) {
+        Dis1 = pins.digitalReadPin(DigitalPin.P8)
+        Dis2 = pins.digitalReadPin(DigitalPin.P1)
+        if (Dis1 == 0 && Dis2 == 1) {
+            break;
+        }
+    }
+    Stop()
+}
 function Stop () {
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P14,
     0,
     DigitalPin.P13,
-    0
+    1
     )
     RoboticsWorkshop.DDMmotor(
     AnalogPin.P16,
@@ -73,25 +270,32 @@ function Stop () {
 }
 let Dis2 = 0
 let Dis1 = 0
-let Start = 0
-radio.setGroup(3)
-Start = 0
-Stop()
+let end = 0
+end = 0
 basic.forever(function () {
-    if (Start == 1) {
-        Dis1 = pins.digitalReadPin(DigitalPin.P8)
-        Dis2 = pins.digitalReadPin(DigitalPin.P1)
-        if (Dis1 == 0 && Dis2 == 0) {
-            Forword()
-        } else if (Dis1 == 1 && Dis2 == 0) {
-            Right()
-        } else if (Dis1 == 0 && Dis2 == 1) {
-            Left()
-        } else if (Dis1 == 1 && Dis2 == 1) {
-            Stop()
-            basic.pause(1000)
-            TurnLeft()
-            basic.pause(500)
-        }
+    if (end == 0) {
+        basic.showLeds(`
+            . # # # .
+            . . . . #
+            . . . . #
+            . . . . #
+            . # # # .
+            `)
+    } else if (end == 1) {
+        basic.showLeds(`
+            # # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (end == 2) {
+        basic.showLeds(`
+            . # . # .
+            . . # # .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
     }
 })
